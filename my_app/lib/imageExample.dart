b3a56661ca5be12edf447ef82d6cc822730bd8c4
lucia <lucia@seektopser.com>
2020-06-03 06:50:39 +0800

    ClipOval

--- my_app/lib/imageExample.dart
+++ my_app/lib/imageExample.dart
@@ -26,19 +26,38 @@ import 'package:flutter/material.dart';
 // }
 
 // 设置背景图
+// class ImageExample extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 300,
+//       width: 300,
+//       decoration: BoxDecoration(
+//         color: Colors.blueAccent,
+//         borderRadius: BorderRadius.circular(150),
+//         image: DecorationImage(
+//           image: NetworkImage('https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'),
+//           )
+//       ),
+//       );
+//   }
+  
+// }
+
+
+// ClipOval按照图片形状，自适应处理成圆形或者椭圆形
 class ImageExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      width: 300,
-      decoration: BoxDecoration(
-        color: Colors.blueAccent,
-        borderRadius: BorderRadius.circular(150),
-        image: DecorationImage(
-          image: NetworkImage('https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'),
-          )
-      ),
+        child: ClipOval(
+          child: Image.network(
+            'http://www.ionic.wang/statics/index/images/ionic4.png',
+            height: 100,
+            width: 100,
+            fit: BoxFit.cover,
+            ),
+        ),
       );
   }
   
