dfd33527ce923e65a1362f270c6d6afd0a2415c3
lucia <lucia@seektopser.com>
2020-07-15 14:18:03 +0800

    padding

--- my_app/lib/consts/listData.dart
+++ my_app/lib/consts/listData.dart
@@ -29,4 +29,34 @@ List listData = [
     'author': 'Google',
     'imageUrl': 'https://www.itying.com/images/flutter/6.png'
   },
+  {
+    'title': 'Chady Shop 1',
+    'author': 'Mohamed Chahin',
+    'imageUrl': 'https://www.itying.com/images/flutter/1.png'
+  },
+  {
+    'title': 'Childhood in a picture 1',
+    'author': 'Google',
+    'imageUrl': 'https://www.itying.com/images/flutter/2.png'
+  },
+  {
+    'title': 'Alibaba shop 1',
+    'author': 'Alibaba',
+    'imageUrl': 'https://www.itying.com/images/flutter/3.png'
+  },
+  {
+    'title': 'Tornado 1',
+    'author': 'Google',
+    'imageUrl': 'https://www.itying.com/images/flutter/4.png'
+  },
+  {
+    'title': 'Chady Shop 1',
+    'author': 'Mohamed Chahin',
+    'imageUrl': 'https://www.itying.com/images/flutter/5.png'
+  },
+  {
+    'title': 'Childhood in a picture',
+    'author': 'Google 1',
+    'imageUrl': 'https://www.itying.com/images/flutter/6.png'
+  },
 ];
--- my_app/lib/padding/paddingExample1.dart
+++ my_app/lib/padding/paddingExample1.dart
@@ -2,29 +2,21 @@ import 'package:flutter/material.dart';
 import '../consts/listData.dart';
 
 class PaddingExample1 extends StatelessWidget {
-  Widget _getList(context, index) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.greenAccent,
-          border: Border.all(color: Colors.blueGrey, width: 3),
-          borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: <Widget>[
-          Text(listData[index]['title']),
-          Image.network(listData[index]['imageUrl']),
-        ],
-      ),
-    );
+  List<Widget> _getList() {
+    var list = listData.map((value) {
+      return Image.network(
+        value['imageUrl'],
+        fit: BoxFit.cover,
+      );
+    });
+    return list.toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: listData.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemBuilder: this._getList,
+    return GridView.count(
+      crossAxisCount: 2,
+      children: this._getList(),
     );
   }
 }
